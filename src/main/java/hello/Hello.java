9ae207df155eef10ca51d67cfac8437a1521ad56
EricKisee <erickiseemulwa@gmail.com>
2023-01-01 19:33:56 +0300

    Add Dependancies

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -1,7 +1,11 @@
 package main.java.hello;
 
+import org.joda.time.LocalTime;
+
 public class Hello {
     public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
         System.out.println("Hello World from Maven");
+        System.out.println("The current local time is: " + currentTime);
     }
 }
